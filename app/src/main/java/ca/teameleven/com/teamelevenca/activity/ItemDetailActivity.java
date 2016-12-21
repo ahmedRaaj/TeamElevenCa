dd31a341f8361796e4f54be008063a23faaab3fe
ZENG-LAPTOP\EvEr <zengxianhan93@gmail.com>
2016-12-21 19:44:42 +0800

    layout

---
+++ app/src/main/java/ca/teameleven/com/teamelevenca/activity/ItemDetailActivity.java
@@ -0,0 +1,15 @@
+package ca.teameleven.com.teamelevenca.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.teameleven.com.teamelevenca.R;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
